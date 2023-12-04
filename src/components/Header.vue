117b8aae7eb455f50cb76a6f13c8a0d4bca7add5
Rilr20 <nrrl99@gmail.com>
2023-12-04 15:54:47 +0100

    removed some css code and used tailwind for it instead

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,20 +6,15 @@ import HelpButton from './HelpButton.vue';
 
 <template>
     <header >
-        <div class="top-div h-10 flex items-center" >
-            <p class="font-bold font-inter text-white ml-48 text-xs">TERRY offert - EN INTERAKTIV OFFERTLÖSNING FRÅN Terry Inc</p>
+        <div class="bg-[#005874] top-div h-10 flex items-center" >
+            <p class="font-bold font-inter text-white ml-96 text-xs">TERRY offert - EN INTERAKTIV OFFERTLÖSNING FRÅN Terry Inc</p>
         </div>
-        <div class="bottom-div h-25 pt-6  px-48 flex justify-between">
+        <div class="bg-[#00B5CC]  bottom-div h-25 pt-6  px-96 flex justify-between">
             <img class="h-10 flex items-center" src="../assets/cloud.png" alt="cloud">
             <help-button />
         </div>
     </header>
 </template>
 <style scoped>
-.top-div {
-    background-color: #005874;
-}
-.bottom-div {
-    background-color: #00B5CC;
-}
+
 </style>
--- src/components/HelpButton.vue
+++ src/components/HelpButton.vue
@@ -6,13 +6,11 @@ import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
 library.add(faCircleQuestion)
 </script>
 <template>
-    <div class="bg-white help-button w-29 px-7 py-2.5 w-40 h-10 rounded-full flex items-center justify-between">
+    <div class=" bg-white help-button w-29 px-7 py-2.5 w-40 h-10 rounded-full flex items-center justify-between text-[#005874]">
         <font-awesome-icon :icon="['fas', 'circle-question']" size="lg" />
         <p class="font-inter font-bold text-xs">Ställ en fråga</p>
     </div>
 </template>
 <style scoped>
-.help-button {
-    color: #005874;
-}
+
 </style>
