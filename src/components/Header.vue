41a9e2b0b9fe9586da45d1509164fdb177a697c7
Rilr20 <nrrl99@gmail.com>
2023-12-04 14:41:08 +0100

    Header design completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,17 @@
 <script setup>
+import HelpButton from './HelpButton.vue';
+
 
 </script>
 
 <template>
-    <header>
-        <div class="top-div h-10 flex items-center">
-            <p class="text-white ml-48">TERRY offert - EN INTERAKTIV OFFERTLÖSNING FRÅN Terry Inc</p>
+    <header >
+        <div class="top-div h-10 flex items-center" >
+            <p class="font-bold font-inter text-white ml-48 text-xs">TERRY offert - EN INTERAKTIV OFFERTLÖSNING FRÅN Terry Inc</p>
         </div>
-        <div class="bottom-div pt-8 h-25">
-
+        <div class="bottom-div h-25 pt-6  px-48 flex justify-between">
+            <img class="h-10 flex items-center" src="../assets/cloud.png" alt="cloud">
+            <help-button />
         </div>
     </header>
 </template>
---
+++ src/components/HelpButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCircleQuestion)
+</script>
+<template>
+    <div class="bg-white help-button w-29 px-7 py-2.5 w-40 h-10 rounded-full flex items-center justify-between">
+        <font-awesome-icon :icon="['fas', 'circle-question']" size="lg" />
+        <p class="font-inter font-bold text-xs">Ställ en fråga</p>
+    </div>
+</template>
+<style scoped>
+.help-button {
+    color: #005874;
+}
+</style>
