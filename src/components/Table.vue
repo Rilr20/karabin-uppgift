750cad05d3a9210388341f65bb8d05cddd704e70
Rilr20 <nrrl99@gmail.com>
2023-12-05 11:51:35 +0100

    table data

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,13 +1,10 @@
 <script setup>
 
-let data = {
-    id: 123452,
-    produkt: "Produkt 1 med en längre beskrivning",
-    count: 200,
-    price: "1 308,00",
-    price2: "483,96",
-    sum: "193 508,40"
-}
+import {defineProps, watch} from 'vue';
+const props = defineProps({
+    data: Array
+});
+
 </script>
 <template>
     <table class="table-fixed w-full font-inter md:text-medium text-xs overflow-x"><!--Table-->
@@ -20,22 +17,13 @@ let data = {
             <th class="md:1/6 w-1/6">Summa</th>
         </thead>
         <tbody class="text-center sm:overflow-auto">
-            <!--Table Row-->
-            <tr class="md:h-14 h-20 md:px-0 px-8 odd:bg-[#F8FDFD] even:bg-white">
-                <td class="p4 md:1/6 w-1/6">{{ data.id }}</td>
-                <td class="p4 text-left md:w-40 w-20 pl-4">Kort titel</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.count }}</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.price }}</td>
-                <td class="p4 2/8">{{ data.price2 }}</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.sum }}</td>
-            </tr>
-            <tr class="md:h-14 h-20 md:px-0 px-8 odd:bg-[#F8FDFD] even:bg-white" v-for="n in 10">
-                <td class="p4 md:1/6 w-1/6">{{ data.id }}</td>
-                <td class="p4 text-left md:w-40 w-20 pl-4">{{ data.produkt }}</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.count }}</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.price }}</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.price2 }}</td>
-                <td class="p4 md:1/6 w-1/6">{{ data.sum }}</td>
+            <tr class="md:h-14 h-20 md:px-0 px-8 odd:bg-[#F8FDFD] even:bg-white" v-for="row in data">
+                <td class="p4 md:1/6 w-1/6">{{ row.id }}</td>
+                <td class="p4 text-left md:w-40 w-20 pl-4">{{ row.description }}</td>
+                <td class="p4 md:1/6 w-1/6">{{ row.ordered_amount }}</td>
+                <td class="p4 md:1/6 w-1/6">{{ row.price }}</td>
+                <td class="p4 md:1/6 w-1/6">{{ row.price2 }}</td>
+                <td class="p4 md:1/6 w-1/6">{{ row.sum }}</td>
             </tr>
 
         </tbody>
